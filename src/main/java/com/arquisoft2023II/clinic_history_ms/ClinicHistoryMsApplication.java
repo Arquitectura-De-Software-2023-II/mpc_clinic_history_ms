d998976be6eeb5f5af933bc2c844fb9f2a997143
Santiago Alvarez Ricardo <santiagoalvarezricardo@gmail.com>
2023-09-10 02:05:15 -0500

    spring base application config: maven - java - spring boot 3.1.3 - jar - java 17 packages: Spring Data MongoDB - Spring Web - Docker Compose Support

---
+++ src/main/java/com/arquisoft2023II/clinic_history_ms/ClinicHistoryMsApplication.java
@@ -0,0 +1,13 @@
+package com.arquisoft2023II.clinic_history_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicHistoryMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicHistoryMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquisoft2023II/clinic_history_ms/ClinicHistoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquisoft2023II.clinic_history_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicHistoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
